a4c8ef5ee5ca259ce386337bb233c5b45c5548a8
Paul Gowder <paul.gowder@gmail.com>
2019-06-16 19:22:25 -0500

    start cleaning saenz

--- automated-repair/fix_word_spacing.py
+++ automated-repair/fix_word_spacing.py
@@ -32,34 +32,23 @@ def correct_line(line):
     if len(line) == 1:
         return line
     words = line.split(" ")
+    out = []
     for word in words:
         correction = split_word(word)
         if correction != word:
             print(word)
             print(correction)
+        out.append(correction)
+    return " ".join(out)
 
 sample_line = """In 1992, California enacted a statute limiting the maximum welfare benefitsavailable to newly arrived residents. The scheme limits the amount payable toa family that has resided in the State for less than 12 months to the amountpayable by the State of the family's prior residence. The questions presentedby this case are whether the 1992 statute was constitutional when it wasenacted and, if not, whether an amendment to the Social Security Act enactedby Congress in 1996 affects that determination.
 """
 
-correct_line(sample_line)
+#print(correct_line(sample_line))
 
-# with open("saenz.md") as saenzin:
-#     lines = saenzin.readlines()
-
-# out = []
-
-# for line in lines:
-#     if len(line) == 1:
-#         out.append(line)
-#     else:
-#         words = line.split(" ")
-        # fixedline = " ".join([split_word(x) for x in words])
-        # out.append(fixedline)
-
-# text = "".join(out)
-# print(text)
-
-#with open("saenz2.md", "w") as saenzout:
-#    lines = saenzin.readlines()
-
-# THIS DOESN'T WORK RIGHT because the wordlist is too short.  It misses words like "constitution." and "court"
+with open("saenz.md") as saenzin:
+    lines = saenzin.readlines()
+out = []
+out = [correct_line(x) for x in lines]
+with open("saenz2.md", "w") as saenzout:
+    saenzout.write("".join(out))
