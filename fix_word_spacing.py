12e2626a62f0e3f3e16a2f69b173ebebdc1a41a1
Paul Gowder <paul.gowder@gmail.com>
2019-06-16 19:15:34 -0500

    trying to fix my fuckups

--- fix_word_spacing.py
+++ fix_word_spacing.py
@@ -7,41 +7,57 @@ with open("words_dictionary.json") as wd:
 
 words = set(words.keys())
 
+
 def find_split(testword):
     for x in range(1, len(testword)):
         first = testword[0:x]
         second = testword[x:]
-        if (first in words) and (second in words):
-            return x
+        if (first in words) or (first.lower() in words):
+            if (second in words) or (second.lower() in words):
+                return x
     return False
 
 def split_word(inword):
-    word = inword.lower()
-    if word in words:
+    word = inword.strip()
+    lower = word.lower()
+    if (word in words) or (lower in words):
         return inword
-    print(word)
     split_index = find_split(word)
     if split_index:
         correction = inword[0:split_index] + " " + inword[split_index:]
-        print(correction)
         return correction
     return inword
 
-with open("saenz.md") as saenzin:
-    lines = saenzin.readlines()
-
-out = []
-
-for line in lines:
+def correct_line(line):
     if len(line) == 1:
-        out.append(line)
-    else:
-        words = line.split(" ")
-        fixedline = " ".join([split_word(x) for x in words])
-        out.append(fixedline)
+        return line
+    words = line.split(" ")
+    for word in words:
+        correction = split_word(word)
+        if correction != word:
+            print(word)
+            print(correction)
 
-#text = "".join(out)
-#print(text)
+sample_line = """In 1992, California enacted a statute limiting the maximum welfare benefitsavailable to newly arrived residents. The scheme limits the amount payable toa family that has resided in the State for less than 12 months to the amountpayable by the State of the family's prior residence. The questions presentedby this case are whether the 1992 statute was constitutional when it wasenacted and, if not, whether an amendment to the Social Security Act enactedby Congress in 1996 affects that determination.
+"""
+
+correct_line(sample_line)
+
+# with open("saenz.md") as saenzin:
+#     lines = saenzin.readlines()
+
+# out = []
+
+# for line in lines:
+#     if len(line) == 1:
+#         out.append(line)
+#     else:
+#         words = line.split(" ")
+        # fixedline = " ".join([split_word(x) for x in words])
+        # out.append(fixedline)
+
+# text = "".join(out)
+# print(text)
 
 #with open("saenz2.md", "w") as saenzout:
 #    lines = saenzin.readlines()
